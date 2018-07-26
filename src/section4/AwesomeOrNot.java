ea5947dcca8bc1c519c6ae37617d405476a57407
CelineOCiotta2006 <league@WorthyMini-5.attlocal.net>
2018-07-26 11:58:12 -0700

    uSRGYi&U

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,10 +2,28 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
+	int cnfvb = new Random().nextInt(4);
+	System.out.println(cnfvb);
+	String v = JOptionPane.showInputDialog(" What is something that you think is awsome");
+	if(cnfvb==0) {
+	JOptionPane.showMessageDialog(null, v+ "  is sooooooooooooooooooooooooo AWSOME!!!!!!");
+	}
+	if(cnfvb==1) {
+	JOptionPane.showMessageDialog(null, v+ "it is ok but it isn't the best");
+	}
+if(cnfvb==2) {
+	JOptionPane.showMessageDialog(null, v+ "  suks so much. It is sad how you think that is awsome");
+}
+if(cnfvb==3) {
+	JOptionPane.showMessageDialog(null, v+ " is so bad that everyone who hears that you think that is awesome will die of disbeleif");
+}
+}
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
